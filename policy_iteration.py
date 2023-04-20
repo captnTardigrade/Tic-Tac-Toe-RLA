51ece75fff4630b56711e91931af902beb5d9db4
Kranthi Sedamaki <skranthi4444@gmail.com>
2023-04-20 16:57:27 +0530

    modularize policy iteration
    - seperate the game from the logic for policy iteration

---
+++ policy_iteration.py
@@ -0,0 +1,328 @@
+# import modules
+import pickle
+import time
+import pygame
+import numpy as np
+from functools import reduce
+from collections import defaultdict
+
+# profiling
+import cProfile
+import pstats
+
+# import typing
+from typing import Tuple, Dict, List, DefaultDict
+import numpy.typing as npt
+
+import tictactoe
+
+SEED = 1337
+
+rng = np.random.default_rng(SEED)
+
+FPS = 60
+
+LINE_WIDTH = 6
+
+SCALING_FACTOR = 100
+
+# define colours
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+DIALOG_COLOR = (205, 180, 219)
+X_MARKER_COLOR = (247, 37, 133)
+O_MARKER_COLOR = (76, 201, 240)
+
+# define font
+font = pygame.font.SysFont("Arial", 40)
+
+# DEBUGGING
+DEBUG_STATE = 10258
+
+
+class PolicyIteration(tictactoe.TicTacToe):
+    """Policy Iteration algorithm for Tic-Tac-Toe"""
+
+    def __init__(
+        self,
+        board_size: int = 3,
+        win_condition: int = 3,
+        gamma: np.float16 = np.float16(0.9),
+    ) -> None:
+        super().__init__(board_size, win_condition, gamma)
+
+        self.index_to_state_map: Dict[int, npt.NDArray[np.int8]] = {}
+        self.state_to_index_map: Dict[tuple, int] = {}
+        self.valid_states = np.zeros(self.num_states, dtype=bool)
+        self.rewards = np.zeros(self.num_states, dtype=np.float16)
+        self.next_states: DefaultDict[int, List[int]] = defaultdict(list)
+
+        # (state, next_state) -> action
+        self.state_next_state_to_action = (
+            np.ones((self.num_states, self.num_states), dtype=tuple) * -1
+        )
+
+        # (state, action) -> next_state
+        self.state_action_to_next_state_map = (
+            np.ones((self.num_states, self.num_cells), dtype=int) * -1
+        )
+
+        self.available_actions_cache: Dict[int, npt.NDArray[np.int8]] = {}
+
+        for i in np.arange(self.num_states, dtype=int):
+            self.index_to_state_map[i] = self.index_to_state(i)
+            state = self.index_to_state_map[i]
+            self.state_to_index_map[tuple(state.flatten())] = i
+            self.valid_states[i] = self.is_valid_state(state)
+            if self.valid_states[i]:
+                self.rewards[i] = self.reward_function(state)
+
+        for i in np.arange(self.num_states, dtype=int):
+            state = self.index_to_state_map[i]
+            if self.valid_states[i]:
+                available_actions = self.available_actions(state)
+                for action in available_actions:
+                    next_state = state.copy()
+                    next_state[action[0]][action[1]] = self.which_players_turn(state)
+                    next_state = tuple(next_state.flatten())
+                    next_state = self.state_to_index_map[next_state]
+                    self.next_states[i].append(next_state)
+
+        for i in np.arange(self.num_states, dtype=int):
+            state = self.index_to_state_map[i]
+            if self.valid_states[i]:
+                available_actions = self.available_actions(state)
+                for action in available_actions:
+                    next_state = state.copy()
+                    next_state[action[0]][action[1]] = self.which_players_turn(state)
+                    next_state = tuple(next_state.flatten())
+                    next_state = self.state_to_index_map[next_state]
+                    self.state_next_state_to_action[i][next_state] = action
+                    action = self.action_to_int(action)
+                    self.state_action_to_next_state_map[i, action] = next_state
+
+        for i in np.arange(self.num_states, dtype=int):
+            state = self.index_to_state_map[i]
+            if self.valid_states[i]:
+                self.available_actions_cache[i] = self.available_actions(state)
+
+        if self.board_size == 3 and self.win_condition == 3:
+            assert self.valid_states.sum() == 5478
+
+    @staticmethod
+    def action_to_int(action: npt.NDArray[np.int8]) -> int:
+        """converts an action to an integer
+
+        Args:
+            action (npt.NDArray[np.int8]): the action
+
+        Returns:
+            int: the integer representation of the action
+        """
+        return 3 * action[0] + action[1]
+
+    def update_values(
+        self,
+        policy: npt.NDArray[np.int8],
+        prev_values: npt.NDArray[np.float16],
+    ) -> npt.NDArray[np.float16]:
+        actions = 3 * policy[:, 0] + policy[:, 1]
+        start = time.time()
+        next_states = self.state_action_to_next_state_map[
+            np.arange(self.num_states), actions
+        ]
+        end = time.time()
+
+        next_states_values = prev_values[next_states]
+        values = self.rewards + self.gamma * next_states_values
+
+        # print(f"Time taken to update values: {end - start}")
+        return values
+
+    def get_policy(
+        self, epsilon: float = 1e-4, max_iterations: int = 100
+    ) -> Tuple[npt.NDArray, npt.NDArray]:
+        """Generates a policy based on value iteration
+
+        Returns:
+            Tuple[npt.NDArray, npt.NDArray]: A tuple of policy and values
+        """
+        policy = np.zeros((self.num_states, 2), dtype=np.int8)
+        values = self.update_values(
+            policy, np.zeros(self.num_states).astype(np.float16)
+        )
+
+        delta = np.inf
+        num_iterations = 0
+        while num_iterations < max_iterations:
+            policy_copy = policy.copy()
+            prev_values = values.copy()
+            values = self.update_values(policy, prev_values)
+            for i in range(self.num_states):
+                start = time.time()
+                state = self.index_to_state_map[i]
+                if not self.valid_states[i]:
+                    continue
+
+                actions = self.available_actions_cache[i]
+                if len(actions) == 0:
+                    continue
+
+                action_values = np.zeros(len(actions))
+                action_ints = 3 * actions[:, 0] + actions[:, 1]
+
+                next_state_indices = self.state_action_to_next_state_map[i, action_ints]
+                action_values = values[next_state_indices]
+
+                if self.which_players_turn(state) == 1:
+                    policy[i] = actions[np.argmax(action_values)]
+                else:
+                    policy[i] = actions[np.argmin(action_values)]
+
+                end = time.time()
+                # print(f"Time taken for iteration: {end - start} seconds")
+            delta = np.max(np.abs(policy_copy - policy))
+            if delta < epsilon:
+                break
+
+            print("iteration:", num_iterations, "delta:", delta)
+            num_iterations += 1
+
+        print(
+            f"Policy iteration convereged to delta: {delta} in {num_iterations} iterations"
+        )
+
+        return policy, values
+
+
+def main(load_policy: bool = False, load_game: bool = False, save_game: bool = False):
+    # initialize pygame
+    if load_game:
+        with open("game.pkl", "rb") as f:
+            game: PolicyIteration = pickle.load(f)
+    else:
+        game = PolicyIteration(3, 3)
+        if save_game:
+            with open("game.pkl", "wb") as f:
+                pickle.dump(game, f)
+
+    if load_policy:
+        with open("policy.npy", "rb") as f:
+            policy = np.load(f)
+    else:
+        policy, _ = game.get_policy()
+        with open("policy.npy", "wb") as f:
+            np.save(f, policy)
+
+    # # set up clock (for efficiency)
+    clock = pygame.time.Clock()
+    # main loop
+    run = True
+    clicked = False
+    while run:
+        clock.tick(FPS)
+        # draw board and markers first
+        game.draw_board()
+        game.draw_markers()
+
+        # handle events
+        for event in pygame.event.get():
+            # handle game exit
+            if event.type == pygame.QUIT:
+                run = False
+
+            if game.game_over:
+                continue
+
+            # run new game
+            if game.player == 1:
+                # check for mouseclick
+                if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
+                    clicked = True
+                if event.type == pygame.MOUSEBUTTONUP and clicked == True:
+                    clicked = False
+                    p_x, p_y = pygame.mouse.get_pos()
+                    cell_x = p_x // SCALING_FACTOR
+                    cell_y = p_y // SCALING_FACTOR
+                    # check if the mouse click is not in the board
+                    if cell_x >= game.board_size or cell_y >= game.board_size:
+                        continue
+                    if game.markers[cell_x][cell_y] == 0:
+                        game.place_marker(cell_x, cell_y)
+                        game_over, winner = game.check_win(
+                            game.markers, game.win_condition
+                        )
+                        game.game_over = game_over
+                        game.winner = winner
+
+            if game.player == -1:
+                # get next move
+                state = game.state_to_index(game.markers)
+                action = policy[state]
+                if action[0] == -1 and action[1] == -1:
+                    print("No action found for state", game.markers)
+                game.place_marker(action[0], action[1])
+                game_over, winner = game.check_win(game.markers, game.win_condition)
+                game.game_over = game_over
+                game.winner = winner
+
+        # check if game has been won
+        if game.game_over == True:
+            game.draw_markers()
+            game.draw_game_over(game.winner)
+            pygame.display.update()
+
+            mouse_clicked = False
+            while not mouse_clicked:
+                event = pygame.event.wait()
+                # check for mouseclick to see if we clicked on Play Again
+                if event.type == pygame.MOUSEBUTTONDOWN and game.clicked == False:
+                    game.clicked = True
+                if event.type == pygame.MOUSEBUTTONUP and game.clicked == True:
+                    game.clicked = False
+                    pos = pygame.mouse.get_pos()
+                    if game.again_rect.collidepoint(pos):
+                        # reset variables
+                        game.reset()
+                mouse_clicked = True
+
+        # update display
+        # game.draw_markers()
+        pygame.display.update()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    DEBUG_STATE = 0
+
+    with cProfile.Profile() as pr:
+        main(load_policy=False, load_game=False)
+
+    # results = pstats.Stats(pr)
+    # results.sort_stats(pstats.SortKey.TIME)
+    # results.print_stats()
+    # results.dump_stats("profile.prof")
+    # game = TicTacToe(3, 3)
+
+    # state = np.array([
+    #     [1, 0, 0],
+    #     [0, 0, 0],
+    #     [-1, 0, 1]
+    # ])
+
+    # print(game.check_win(state, 3))
+
+    # print(game.which_players_turn(state))
+
+    # state_index = game.state_to_index(state)
+
+    # DEBUG_STATE = state_index
+
+    # policy, values = game.get_policy()
+
+    # print(values[state_index])
+
+    # print(policy[state_index])
